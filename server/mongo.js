e8ea3b112499fabed3a4ca2cf9e4e2e940045e79
Vandervort <hvandervort1@una.edu>
2026-09-22 13:25:26 -0500

    made changes

--- server/mongo.js
+++ server/mongo.js
@@ -1,10 +1,10 @@
 import 'dotenv/config';
-
+import express from 'express'
 import {MongoClient, ServerApiVersion} from 'mongodb';
 // const uri = "mongodb+srv://<JohnSmith>:<Jack1101>@cluster0.sm7l3lx.mongodb.net/?appName=Cluster0";
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const uri = process.env.MONGO_URI;
-
+const app = express();
 const client = new MongoClient(uri, {
 
 
@@ -23,3 +23,18 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
+app.get('/api/hello', function(req, res) {
+    const message = {
+      message : 'hello from hard code json',
+      success : true
+    };
+    res.json(message);
+
+  }
+);
+
+app.listen(5500, () => {
+  console.log('Server is running on http://localhost:5500')
+})
